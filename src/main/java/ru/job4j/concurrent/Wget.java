3b83398ab159bf0a19ab5cb573de8e90689e61e7
Korolenko Sergey <pavilion1900@gmail.com>
2022-07-23 14:13:26 +0300

    1.2. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,19 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        for (int i = 0; i <= 100; i++) {
+                            Thread.sleep(1000);
+                            System.out.print("\rLoading: " + i + "%");
+                        }
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+    }
+}
