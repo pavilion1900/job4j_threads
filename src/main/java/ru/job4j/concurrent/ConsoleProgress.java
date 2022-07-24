3c3f3eea2c591dd526eda74e5278eaad344ddc89
Korolenko Sergey <pavilion1900@gmail.com>
2022-07-24 23:42:09 +0300

    3. Прерывание нити [#1019 #325489]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -4,12 +4,14 @@ public class ConsoleProgress implements Runnable {
 
     @Override
     public void run() {
+        int index = 0;
+        char[] array = {'-', '\\', '|', '/'};
         while (!Thread.currentThread().isInterrupted()) {
             try {
-                System.out.print("\rLoading ... -");
-                System.out.print("\rLoading ... \\");
-                System.out.print("\rLoading ... |");
-                System.out.print("\rLoading ... /");
+                System.out.print("\rLoading ... " + array[index++]);
+                if (index == array.length) {
+                    index = 0;
+                }
                 Thread.sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
