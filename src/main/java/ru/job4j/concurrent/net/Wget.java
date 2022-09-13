b0ce84e7a192f34af472aad957cc2927b0505aa4
Korolenko Sergey <pavilion1900@gmail.com>
2022-09-13 15:20:34 +0300

    4. Скачивание файла с ограничением. [#144271]

---
+++ src/main/java/ru/job4j/concurrent/net/Wget.java
@@ -0,0 +1,67 @@
+package ru.job4j.concurrent.net;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.net.URL;
+
+public class Wget implements Runnable {
+
+    private final String url;
+    private final int speed;
+    private final String fileName;
+
+    public Wget(String url, int speed, String fileName) {
+        this.url = url;
+        this.speed = speed;
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            int bytesWrite = 0;
+            long start = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                bytesWrite += bytesRead;
+                if (bytesWrite >= speed) {
+                    long duration = System.currentTimeMillis() - start;
+                    long sleepTime = bytesWrite / speed * 1000L - duration;
+                    Thread.sleep(sleepTime);
+                    bytesWrite = 0;
+                    start = System.currentTimeMillis();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void validate(String[] args) {
+        if (args.length < 3) {
+            throw new IllegalArgumentException("Not enough arguments");
+        }
+        if (args[0] == null) {
+            throw new IllegalArgumentException("Not enough arguments. Url is empty");
+        }
+        if (args[1] == null) {
+            throw new IllegalArgumentException("Not enough arguments. Speed is empty");
+        }
+        if (args[2] == null) {
+            throw new IllegalArgumentException("Not enough arguments. File name is empty");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        validate(args);
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        String userName = args[2];
+        Thread wget = new Thread(new Wget(url, speed, userName));
+        wget.start();
+        wget.join();
+    }
+}
